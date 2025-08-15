35e74c3283ba820d1d60c9f46aec0a4e2a05303d
eduardoDC101 <edutnb2018@gmail.com>
2025-08-15 11:46:48 -0300

    Tema Escuro

--- HTML/ProjetoLogin/script.js
+++ HTML/ProjetoLogin/script.js
@@ -1,7 +1,6 @@
 /* Mostrando Senha */
-
-let inputSenha = document.getElementById('password');
-let iconeSenha = document.getElementById('seePassword');
+const inputSenha = document.getElementById('password');
+const iconeSenha = document.getElementById('seePassword');
 
 iconeSenha.addEventListener('click', () => {
     let verificarTipo = inputSenha.type === "password";
@@ -11,6 +10,24 @@ iconeSenha.addEventListener('click', () => {
     iconeSenha.innerHTML = verificarTipo 
     ? "<i class='fa-solid fa-eye-slash'></i>"
     : "<i class='fa-solid fa-eye'></i>";
-})
-
+});
 /* Mostrando Senha */
+
+
+/* Tema Dark */
+const botaoTema = document.getElementById('tema-dark');
+
+botaoTema.addEventListener('click' , () =>{
+    let tagHtml = document.documentElement;
+
+    let atributoHtml = tagHtml.getAttribute('data-theme');
+
+    tagHtml.setAttribute('data-theme', atributoHtml === "light" ? "dark" : "light");
+
+    botaoTema.innerHTML = atributoHtml === "light"
+    ? "<i class='fa-solid fa-sun'></i>"
+    : "<i class='fa-solid fa-moon'>";
+
+});
+
+/* Tema Dark */
